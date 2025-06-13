388af23d11199b279d62ad53d9974b3bdaacdfce
ThrishikK <kosuruthrishik@gmail.com>
2025-06-13 08:54:29 +0530

    Minor CHanges

--- Movies_And_Shows/series.sql
+++ Movies_And_Shows/series.sql
@@ -16,7 +16,9 @@ INSERT INTO
     series
 		(id,name,genre,rating,creators,no_of_episodes,first_episode_date,no_of_seasos)
     VALUES
-		(1,'Stranger Things','Horror',8.6,'Matt Duffer,Ross Duffer',34,'2016-07-15',4),
+		    (1,'Stranger Things','Horror',8.6,'Matt Duffer,Ross Duffer',34,'2016-07-15',4),
         (2,'Dark','Thriller',8.7,'Baran bo Odar, Jantje Friese',26,'2017-12-01',3),
-        (3,'Modern Family','Sitcom',8.5,'Christopher Lloyd, Steven Levitan',250,'2009-09-23',11);
-    
+        (3,'Modern Family','Sitcom',8.5,'Christopher Lloyd, Steven Levitan',250,'2009-09-23',11),
+        (4,'Friends','Sitcom',8.9,'Marta Kauffman, David Crane',236,'1994-09-22',10),
+        (5,'Seinfeld','Sitcom',8.9,'Jerry Seinfeld, Larry David',180,'1989-07-05',9),
+        (6,'Nancy Drew','Thriller',6.7,'Edward Stratemeyer',62,'2019-10-09',4);
