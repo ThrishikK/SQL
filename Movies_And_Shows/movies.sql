0ee33ef4137ce6db278b8d81cf80684381932e88
ThrishikK <kosuruthrishik@gmail.com>
2025-06-15 07:03:17 +0530

    Added 3 more movies

--- Movies_And_Shows/movies.sql
+++ Movies_And_Shows/movies.sql
@@ -21,7 +21,13 @@ VALUES
 (2,'The Incredible Hulk','Action/Sci-fi','Dr Bruce Banner subjects himself to high levels of gamma radiation, which triggers his transformation into a huge green creature, the Hulk, whenever he experiences negative emotions such as anger.
 ',6.6,'2008-06-20','Louis Leterrier',FALSE,'01:52'),
 (3,'Analyze This','Comedy/Crime','Ben Sobol, a psychologist, faces several problems in his personal life. One day, when he crashes into a mob boss\'s car, the dangerous criminal pays him a visit and shares his inner conflicts with him.
-',6.7,'1999-03-05',' Harold Ramis',FALSE,'01:43')
+',6.7,'1999-03-05',' Harold Ramis',FALSE,'01:43'),
+(4,'Zoolander','Comedy','Derek, a model, loses hope when his rival wins a coveted title. In a bid to revive his career, a designer offers him a runway show but ends up convincing him to kill the prime minister of Malaysia.',6.5,'2001-09-28','Ben Stiller',TRUE,'01:29'
+),
+(5,'Man of Steel','Action/Sci-fi','Clark learns about the source of his abilities and his real home when he enters a Kryptonian ship in the Artic. However, an old enemy follows him to Earth in search of a codex and brings destruction.
+',7.1,'2013-06-14','Zack Snyder',TRUE,'02:23'),
+(6,'Reality Bites','Comedy/Romance','Lelaina, Troy, Vickie and Sammy are friends who stay together. Lelaina wants be a filmmaker and makes her friends act for her film. Life gets complex when they have to handle career, love and family.
+',6.6,'1994-02-18','Ben Stiller',FALSE,'01:39');
 
 
 
