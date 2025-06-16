1e6f562f91afb7e89216a251c71366a2a6b7c557
ThrishikK <kosuruthrishik@gmail.com>
2025-06-16 07:33:57 +0530

    Actors and Movies Joint table created

---
+++ Movies_And_Shows/actors_in_movies.sql
@@ -0,0 +1,24 @@
+CREATE TABLE actors_in_movies(
+  id INT,
+  actor_id INT,
+  movie_id INT,
+  FOREIGN KEY (actor_id) 
+  REFERENCES actors(id),
+  FOREIGN KEY (movie_id)
+  REFERENCES movies(id),
+  PRIMARY KEY(actor_id,movie_id)
+)
+
+INSERT INTO actors_in_movies
+(id,actor_id,movie_id)
+VALUES 
+(1,1,1),
+		(2,3,2),
+		(3,10,3),
+		(4,12,4),
+		(5,9,5),
+		(6,7,6),
+    (7,2,7),
+    (8,4,3),
+    (9,8,2),
+    (10,5,4);
