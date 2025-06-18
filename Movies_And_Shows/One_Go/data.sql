e41e34e6e110c8ba65a9e5e28447502012bd92b5
ThrishikK <kosuruthrishik@gmail.com>
2025-06-18 19:16:09 +0530

    Some More Data added

--- Movies_And_Shows/One_Go/data.sql
+++ Movies_And_Shows/One_Go/data.sql
@@ -16,7 +16,10 @@ VALUES
     (12,'Jerry Stiller','1927-06-08','America'),
     (13,'Steve Carell','1962-08-16','America'),
     (14,'Craig Robinson','1971-10-25','America'),
-    (15,'Andy Samberg','1978-07-18','America');
+    (15,'Andy Samberg','1978-07-18','America'),
+    (16,'Stephanie Beatriz','1981-02-10','Argentine'),
+    (17,'Jim Parsons','1973-03-24','America'),
+    (18,'Brian George','1952-07-01','British');
 
 
 -- SERIES TABLE
@@ -32,7 +35,8 @@ INSERT INTO
         (5,'Seinfeld','Sitcom',8.9,'Jerry Seinfeld, Larry David',180,'1989-07-05',9),
         (6,'Nancy Drew','Thriller',6.7,'Edward Stratemeyer',62,'2019-10-09',4),
         (7,'The Office','Sitcom',9,'Greg Daniels; Ricky Gervais',201,'2005-03-24',9),
-        (8,'Brooklyn Nine-Nine','Sitcom',8.4,'Michael Schur, Dan Goor, Carly Hallam, Norm Hiscock',153,'2013-09-17',8);
+        (8,'Brooklyn Nine-Nine','Sitcom',8.4,'Michael Schur, Dan Goor, Carly Hallam, Norm Hiscock',153,'2013-09-17',8),
+        (9,'The Big Bang Theory','Sitcom',8.1,'Chuck Lorre, Bill Prady',279,'2007-09-24',12);
 
     -- ACTORS IN SERIES TABLE
     
@@ -56,7 +60,12 @@ INSERT INTO
         (14,13,7),
         (15,14,7),
         (16,14,8),
-        (17,15,8);
+        (17,15,8),
+        (18,16,3),
+        (19,16,8),
+        (20,17,9),
+        (21,18,5),
+        (22,18,9);
         
     -- MOVIES TABLE
     
